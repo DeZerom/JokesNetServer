a6f650b87726d916588263fb5d9bf788359e30e8
DeZerom <demid2001s@gmail.com>
2022-07-11 00:46:53 +0300

    Profile changes

--- src/main/kotlin/ru/dezerom/features/profile/Profile.kt
+++ src/main/kotlin/ru/dezerom/features/profile/Profile.kt
@@ -2,15 +2,14 @@ package ru.dezerom.features.profile
 
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import ru.dezerom.features.auth.check_token.Token
+import ru.dezerom.utils.AUTH_HEADER
 
 fun Application.configureProfile() {
     routing {
         post("/profile") {
-            val token = call.receive<Token>()
+            val token = call.request.headers[AUTH_HEADER] ?: ""
             val controller = ProfileController()
 
             val profileInfo = try {
--- src/main/kotlin/ru/dezerom/features/profile/ProfileController.kt
+++ src/main/kotlin/ru/dezerom/features/profile/ProfileController.kt
@@ -1,18 +1,15 @@
 package ru.dezerom.features.profile
 
-import io.ktor.http.*
 import ru.dezerom.databse.auth.token.TokenModel
-import ru.dezerom.databse.auth.user.UserModel
 import ru.dezerom.databse.jokes.JokeModel
-import ru.dezerom.features.auth.check_token.Token
 
 class ProfileController {
 
     /**
      * @return [ProfileInfo] or null if no such token
      */
-    suspend fun getProfileInfo(token: Token): ProfileInfo? {
-        val tokenDTO = TokenModel.selectToken(token.token) ?: return null
+    suspend fun getProfileInfo(token: String): ProfileInfo? {
+        val tokenDTO = TokenModel.selectToken(token) ?: return null
         val jokesAdded = JokeModel.countUsersJokes(tokenDTO.userLogin)
 
         return ProfileInfo(
